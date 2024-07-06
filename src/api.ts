ed8d6e8bacbb9367067e0d4fb961dc03a37228e2
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-07-06 15:12:57 +0200

    Fix api not exported as default

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import { OneInchApi } from './1inch/api.js'
 
-export class SwapApiRouter {
+export default class SwapApiRouter {
   constructor(target, key) {
     if (target === '1inch') return new OneInchApi(key)
   }
