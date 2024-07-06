910df9087b67930fbc1384dd7766da403b64f777
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-07-06 15:36:11 +0200

    Prepare API to support more API's :trollface:

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,29 @@
 import { OneInchApi } from './1inch/api.js'
 
 export default class SwapApiRouter {
+  api: OneInchApi
   constructor(target, key) {
-    if (target === '1inch') return new OneInchApi(key)
+    if (target === '1inch') this.api = new OneInchApi(key)
+  }
+
+  get tokens() {
+    return this.api.tokens
+  }
+  get quote() {
+    return this.api.quote
+  }
+
+  get swap() {
+    return this.api.swap
+  }
+
+  get routerAddres() {
+    return this.api.routerAddres
+  }
+  get approve() {
+    return this.api.approve
+  }
+  get allowance() {
+    return this.api.allowance
   }
 }
